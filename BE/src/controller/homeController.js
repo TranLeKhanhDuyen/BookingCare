c476c39436700ee0353c4afc99de9b245e5f1551
Duyen <trankhanhduyen312@gmail.com>
2024-02-28 16:37:31 +0700

    feat: connect db

--- BE/src/controller/homeController.js
+++ BE/src/controller/homeController.js
@@ -1,4 +1,12 @@
-const getHomePage = (req, res) => {
+import db from '../models/index';
+
+const getHomePage = async (req, res) => {
+  try {
+  } catch (e) {
+    console.log(e);
+  }
+
+  const data = await db.user.findAll();
   return res.send('homepage.ejs');
 };
 
