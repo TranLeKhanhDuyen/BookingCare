bd01a97bb4000b1525edfd8229f13c2aa79fad9e
tranlekhanhduyen <trankhanhduyen312@gmail.com>
2024-03-07 11:27:02 +0700

    feat: create api login

--- BE/src/server.js
+++ BE/src/server.js
@@ -3,10 +3,12 @@ import bodyParser from 'body-parser'; //Get the correct id sent by the user
 import viewEngine from './config/viewEngine';
 import initWebRoutes from './route/web';
 import connectDB from './config/connectDB';
+import cors from 'cors';
 
 require('dotenv').config();
 
 const app = express();
+app.use(cors({ origin: true }));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
